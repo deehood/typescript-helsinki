fc2685f30e7aeb674d721c031b77b7926d1b4569
deehood <dee.hood@gmail.com>
2022-09-12 13:15:58 +0100

    9.1 Body mass index

--- first-exercises/bmiCalculator.ts
+++ first-exercises/bmiCalculator.ts
@@ -1,1 +1,8 @@
-function calculateBmi(height: Number, weight: Number) {}
+function calculateBmi(height: number, weight: number): string {
+    const result = weight / (height / 100) ** 2;
+
+    if (result < 18.5) return "underweight";
+    if (result > 25) return "overweight";
+    return "Normal (healthy weight)";
+}
+console.log(calculateBmi(180, 74));
