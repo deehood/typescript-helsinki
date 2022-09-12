ce18c4de2615e319ed717c09a42e0581b03ee8b4
deehood <dee.hood@gmail.com>
2022-09-12 18:11:39 +0100

    bmi arguments

--- first-exercises/bmiCalculator.ts
+++ first-exercises/bmiCalculator.ts
@@ -1,3 +1,18 @@
+interface inputValues {
+    value1: number;
+    value2: number;
+}
+
+const parseArguments = (args: Array<string>): inputValues => {
+    if (args.length < 4) throw new Error("Not enough arguments");
+    if (args.length > 4) throw new Error("Too many arguments");
+    if (isNaN(Number(args[2])) || isNaN(Number(args[3])))
+        throw new Error("i need numbers");
+    if (Number(args[2]) > 0 && Number(args[3]) > 0)
+        return { value1: Number(args[2]), value2: Number(args[3]) };
+    throw new Error("i need positive numbers");
+};
+
 function calculateBmi(height: number, weight: number): string {
     const result = weight / (height / 100) ** 2;
 
@@ -5,4 +20,12 @@ function calculateBmi(height: number, weight: number): string {
     if (result > 25) return "overweight";
     return "Normal (healthy weight)";
 }
-console.log(calculateBmi(180, 74));
+
+try {
+    const { value1, value2 } = parseArguments(process.argv);
+    console.log(calculateBmi(value1, value2));
+} catch (error) {
+    let msg = "there was an error. ";
+    if (error instanceof Error) msg += `error : ${error.message}`;
+    console.log(msg);
+}
