8a6f5a8187d57ee365da5d52fd00dc8c3582a670
deehood <dee.hood@gmail.com>
2022-09-19 20:40:50 +0100

    9.9: Patientor backend, step2

--- Patientor/index.ts
+++ Patientor/index.ts
@@ -1,8 +1,10 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.get("/api/ping", (_req, res) => {
     res.send("pong");
