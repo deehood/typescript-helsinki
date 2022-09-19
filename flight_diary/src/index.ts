96a053b76fd9325bc56bd6e19ea39065d95830a8
deehood <dee.hood@gmail.com>
2022-09-19 23:25:29 +0100

    resolveJsonModule in tsconfig

--- flight_diary/src/index.ts
+++ flight_diary/src/index.ts
@@ -1,12 +1,19 @@
 import express from "express";
+import diaryRouter from "../src/routes/diaries";
+import cors from "cors";
 const app = express();
 app.use(express.json());
+// eslint-disable-next-line @typescript-eslint/no-unsafe-call
+app.use(cors());
 const PORT = 3005;
 
 app.get("/ping", (_req, res) => {
     console.log("someone pinged us");
     res.send("pong");
 });
+
+app.use("/api/diaries", diaryRouter);
+
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
