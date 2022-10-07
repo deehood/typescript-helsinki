8d4c66fb680430433b5846caf8c332cab34da508
deehood <dee.hood@gmail.com>
2022-10-07 13:14:48 +0100

    cleanup  diagnoseServices

--- Patientor/backend/services/diagnoseService.ts
+++ Patientor/backend/services/diagnoseService.ts
@@ -7,21 +7,6 @@ const getDiagnoses = (): Array<Diagnosis> => {
     return diagnosis;
 };
 
-const getDiagnosisData = (diagnosisCodes: Array<string>): Array<Diagnosis> => {
-    let found: Diagnosis | undefined;
-
-    const diagArray = diagnosisCodes.reduce(
-        (accArray: Array<Diagnosis>, diagCode) => {
-            found = diagnoseData.find((fields) => fields.code === diagCode);
-
-            return found ? accArray.concat(found) : accArray;
-        },
-        []
-    );
-    return diagArray;
-};
-
 export default {
     getDiagnoses,
-    getDiagnosisData,
 };
